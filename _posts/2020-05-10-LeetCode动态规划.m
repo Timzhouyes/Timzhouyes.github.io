de7923176e74f4e61e6342ae3683e087de291669
Timzhouyes <1175566718@qq.com>
2020-05-11 20:44:10 +0800

    Create 2020-05-10-LeetCode动态规划.m

---
+++ _posts/2020-05-10-LeetCode动态规划.m
@@ -0,0 +1,24 @@
+---
+layout:     post   				    # 使用的布局（不需要改）
+title:      LeetCode 动态规划 Java实现 		# 标题 
+subtitle:   包含题解和想法        #副标题
+date:       2020-05-11		# 时间
+author:     Haiming 						# 作者
+header-img: img/post-bg-2015.jpg 	#这篇文章标题背景图片
+catalog: true 						# 是否归档
+tags:								#标签
+    - Programming
+    - LeetCode
+    - Dynamic programming
+---
+
+基于参考，进行自己的解读和代码编写。
+
+动态规划，要将问题进行转换，从底到顶的进行求解。同时，要能将状态的转换抽象出状态转移方程，将方程之间的状态流转表示出来。
+
+1. 
+
+还是参照大佬的答案：
+
+[https://github.com/CyC2018/CS-Notes/blob/master/notes/Leetcode%20%E9%A2%98%E8%A7%A3%20-%20%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92.md](https://github.com/CyC2018/CS-Notes/blob/master/notes/Leetcode 题解 - 动态规划.md)
+
